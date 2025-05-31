97fecc497f39b70bdbc6350f31e4701d555748e3
MuhammadAmin02 <muhammadamininto011@gmail.com>
2025-05-31 15:59:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+//1)
+//let a = Math.random ()*10
+//alert(`${Math.round(a)} `)
+
+//2)
+//let a = Math.floor(12.510)
+//alert(Math.floor(a))
+
+
+//3)
+
+//function harflarSoni() {
+  //  let text = "MARS IT SCHOOL";
+    //let uzunlik = text.replace();
+    //alert(uzunlik.length);
+  //}
+
+//4 )
+//console.log("MARS IT SCHOOL".length)
+
+ 
+// 5)
+// for (let i = 1; i < 11; i++) {
+//   console.log(i,"MARS IT SCHOOL")
+    
+// }
+
+  
+
+// 6)
+ //let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+
+
+// Javob:Kodini yozib bering
+//console.log(harflar[11]+harflar[19]+harflar[6]+harflar[0]+harflar[11]+ harflar[11]+harflar[0]+harflar[2]+harflar[0]+harflar[11]+harflar[7]+harflar[12]);
+
+
+
+// 7)
+
+    // let yosh=prompt("yoshingizni kiriting:");
+    // if(yosh>18){
+       //  alert(" kattasiz")
+     //}else if(yosh<18){
+       //  alert("Kichiksiz")
+    // }else{
+      //   alert("yoshsiz")
+ //}
+
+
+
+// 8)
+
+// let ism=prompt("ism kiriting");
+// console.log(ism.split("").reverse().join(""));
+
+// 9)
+
+//let a = document.querySelector('.li');  
+// let b = document.querySelector('.lii');  
+// let d = document.querySelector('.liii');  
+
+// let c = prompt('Your name');  
+// let e = prompt('Your surname');  
+// let y = +prompt('Your age');  
+
+// a.textContent = "Name: " + c;  
+// b.textContent = "Surname: " + e;  
+// d.textContent = "Age: " + y;  
+
+// a.style.color = "black";  
+// b.style.color = "black";  
+// d.style.color = "black";
+// 10-savol Array metodlari
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+// Javob :Kodini yozib bering
+//let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//let juftlar = sonlar.filter(son => son % 2 === 0);
+//console.log(juftlar);  [2, 4, 6, 8, 10]
+
+
+
